3156cbd6ad366dd55b0006a0173d55e9ae9857cf
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-08-13 18:56:12 +0200

    v1

--- zerobot/classexposer.py
+++ zerobot/classexposer.py
@@ -6,6 +6,7 @@ import queue
 import traceback
 import inspect
 
+#signal.signal(signal.SIGINT, signal_handler)
 class ClassExposer(Client):
 	"""
 	Permet d'exposer les méthodes d'une classe à distance. Les requêtes sont
@@ -66,7 +67,7 @@ class ClassExposer(Client):
 		return "ClassExposerWorker(%s,%s,%s,..)" % (self.identity, self.conn_addr, self.exposed_obj)
 		
 
-class AsyncClassExposer(Proxy):
+class AsyncClassExposer(Base):
 	"""
 	Permet d'exposer les méthodes d'une classe à distance. Permet en plus
 	de lancer plusieurs méthodes bloquantes de la classe simultanément.
@@ -83,40 +84,45 @@ class AsyncClassExposer(Proxy):
 		@param {int} min_workers si non précisé est égale à init_workers
 		@param {bool|False} dynamic_workers autorisé l'ajout/suppression de workers automatiquement
 		"""
-		super(AsyncClassExposer,self).__init__(identity, ctx=ctx,
-			ft_conn_addr=conn_addr, ft_type=zmq.DEALER,
-			bc_bind_addr="inproc://"+identity, bc_type=zmq.ROUTER)
+		# zmq context
+		self.ctx = ctx or zmq.Context()
+		self._ctx_is_mine = ctx is None
+		# sauvegarde de l'identité
+		self.identity = identity
+		# création ds sockets
+		self.frontend = self.ctx.socket(zmq.DEALER)
+		self.frontend.setsockopt(zmq.IDENTITY, self.identity.encode())
+		self.backend = self.ctx.socket(zmq.ROUTER)
+		self.backend.setsockopt(zmq.IDENTITY, self.identity.encode())
+		# sauvegarde des adresses
+		self._ft_addr = conn_addr
+		self._bc_addr = "inproc://workers-%s"%self.identity
+		# bind/connect
+		self.frontend.connect(self._ft_addr)
+		self.backend.bind(self._bc_addr)
+		# poller
+		self.poller = zmq.Poller()
+		self.poller.register(self.frontend, zmq.POLLIN)
+		self.poller.register(self.backend, zmq.POLLIN)
+		#
 		self.exposed_obj = exposed_obj
 		self.min_workers = min_workers or init_workers
 		self.max_workers = max_workers
 		self.dynamic_workers = dynamic_workers
 		# workers
 		self._workers = {}
+		self._n_workers = 0
 		self._free_workers = []
+		self._n_free_workers = 0
 		for _ in range(init_workers):
 			self.add_worker()
 		self._timeout_can_reduce_workers = 0
 		#self.loop = FdLoop({self.backend: self.backend_handler, self.frontend: self.frontend_handler})
-		# msg queue
-		self._unprocess_msg = queue.deque()
-
-	def add_worker(self):
-		worker_id = "Worker-%s-%s" % (self.identity, uuid.uuid1())
-		worker = ClassExposer(worker_id, self._bc_addr, self.exposed_obj, ctx=self.ctx)
-		# démarage en mode non bloquant pour qu'ils soient dans des threads
-		worker.start(False)
-		self._free_workers.append(worker_id)
-		self._workers[worker_id] = worker
-
-	def backend_process_msg(self, msg):
-		self.logger.debug("backend recv %s", msg)
-		worker_id, msg = msg[0], msg[1:]
-		worker_id = worker_id.decode()
-		self._free_workers.append(worker_id)
-		self.consume_unprocess_msg()
-		self.logger.debug("send to frontend %s",msg)
-		return msg
-
+		# events
+		self._e_stop = threading.Event()
+		# logger
+		self.logger = logging.getLogger(__name__+'.'+self.__class__.__name__)
+	"""
 	def consume_unprocess_msg(self):
 		# ajouter des workers si on galère trop
 		if self.dynamic_workers:
@@ -148,30 +154,107 @@ class AsyncClassExposer(Proxy):
 						self.add_worker()
 					self._timeout_can_reduce_workers = time.time()+10
 					self.logger.info("%s grows to %s workers", self.identity, len(self._workers))
+	"""
 	
-	def send_to_worker(self, worker_id, msg):
-		new_msg = [worker_id.encode()]+msg
-		self.logger.debug("send to backend %s"% new_msg)
-		self.backend.send_multipart(new_msg)
+	def _loop(self):
+		while not self._e_stop.is_set():
+			frontend,backend = self.frontend,self.backend
+			try:
+				socks = dict(self.poller.poll())
+			except Exception as ex:
+				if not self._e_stop.is_set():
+					self.logger.error(ex, exc_info=True)
+				else: continue
+
+			# Handle worker activity on backend
+			if (backend in socks and socks[backend] == zmq.POLLIN):
+				msg = backend.recv_multipart()
+				new_msg = self.backend_process_msg(msg)
+				if new_msg:
+					self.logger.debug("send to frontend %s",msg)
+					self.frontend.send_multipart(new_msg)
+			
+			# poll on frontend only if workers are available
+			if len(self._free_workers) > 0:
+				if (frontend in socks and socks[frontend] == zmq.POLLIN):
+					msg = frontend.recv_multipart()
+					new_msg = self.frontend_process_msg(msg)
+					if new_msg:
+						self.logger.debug("send to backend %s",msg)
+						self.backend.send_multipart(new_msg)
+				if self.dynamic_workers and len(self._free_workers) > 0:
+					self.ungrow()
+			elif self.dynamic_workers:
+				self.grow()
+			
+	def grow(self):
+		# ajouter des workers si on galère trop
+		if self._n_workers != self.max_workers:
+			for i in range(self._n_workers, min(self.max_workers,2*self._n_workers)):
+				self.add_worker()
+			self._timeout_can_reduce_workers = time.time()+10
+			self.logger.info("%s grows to %s workers", self.identity, self._n_workers)
+
+	def ungrow(self):
+		# retirer des workers si on en a trop
+		if self._n_workers > self.min_workers and self._n_free_workers > self._n_workers//2 and time.time()>self._timeout_can_reduce_workers:
+			max_to_remove = self._n_workers-self.min_workers
+			for i in range(min(max_to_remove, self._n_free_workers//2)):
+				worker_id = self._free_workers.pop()
+				self._workers[worker_id].stop()
+				self._workers[worker_id] = None
+				del self._workers[worker_id]
+			self.logger.info("%s ungrows %s workers", self.identity,self._n_workers)
+			self._timeout_can_reduce_workers = time.time()+10
+	
+	def start(self, block=True):
+		self.logger.info("%s started", self)
+		if block:
+			self._loop()
+		else:
+			t = threading.Thread(target=self._loop,name="%s._loop"%self)
+			t.setDaemon(True)
+			t.start()
+
+	def stop(self):
+		self.logger.info("stop event received")
+		self._e_stop.set()
+
+	def close(self):
+		self.logger.info("close event received")
+		self.stop()
+		for worker in self._workers.values():
+			worker.close()
+		self.frontend.close()
+		self.backend.close()
+		if self._ctx_is_mine:
+			self.ctx.term()
+		self.logger.info("closed")
+	
+	def add_worker(self):
+		worker_id = "Worker-%s-%s" % (self.identity, uuid.uuid1())
+		worker = ClassExposer(worker_id, self._bc_addr, self.exposed_obj, ctx=self.ctx)
+		# démarage en mode non bloquant pour qu'ils soient dans des threads
+		worker.start(False)
+		self._free_workers.append(worker_id)
+		self._workers[worker_id] = worker
+		self._n_workers += 1
+		self._n_free_workers += 1
+
+	def backend_process_msg(self, msg):
+		self.logger.debug("backend recv %s", msg)
+		worker_id, msg = msg[0], msg[1:]
+		worker_id = worker_id.decode()
+		self._free_workers.append(worker_id)
+		self._n_free_workers += 1
+		return msg
 			
 	def frontend_process_msg(self, msg):
 		#print('ClassExposer %s received: %s' % (self.identity, msg))
 		self.logger.debug("frontend recv %s", msg)
-		self._unprocess_msg.append(msg)
-		self.consume_unprocess_msg()
-		return None
-
-	def help(self, method=None):
-		"""
-		Permet d'afficher de l'aide sur la classe exposée, utilise la doc python du code source.
-		"""
-		if not method:
-			methods = inspect.getmembers(self.exposed_obj, predicate=inspect.ismethod)
-			r =  map(lambda x: x[0], methods)
-			r = list(filter(lambda x: not x.startswith('_'), r))
-		else:
-			r = dict(inspect.getfullargspec(getattr(self.exposed_obj,method))._asdict())
-		return r
+		worker_id = self._free_workers.pop()
+		self._n_free_workers -= 1
+		return [worker_id.encode()]+msg
 
 	def __repr__(self):
 		return "AsyncClassExposer(%s,%s,%s,..)" % (self.identity, self._ft_addr, self.exposed_obj)
