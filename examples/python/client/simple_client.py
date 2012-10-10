78cbf2de6bf63cf8bd8a1951a1f7ae2afa37c3a2
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-10-10 23:58:06 +0200

    ajout de quelques examples en python

---
+++ examples/python/client/simple_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../../../python3')
+import zerobot
+
+client = zerobot.Client("myclient", "tcp://localhost:5000", "myservice")
+
+# lancement du thread du client, a voir si ca ne doit pas etre fait automatiquement
+# dans le __init__
+client.start(False)
+
+print('ping', client.ping(42))
+print('time', client.time())
+
+try:
+	print(client.nexistepas())
+except Exception as ex:
+	print(ex)
+
+print('doc global', client.help())
+print('doc time :', client.help('time'))
+print('doc cool :', client.help('cool'))
+print('doc nexistepas :', client.help('nexistepas'))
---
+++ examples/python/service/simple_service.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+import sys
+sys.path.append('../../../python3')
+import zerobot
+import time
+
+
+
+
+class MyService:
+	def ping(self, num):
+		""" Pong ? """
+		return num+42
+
+	def time(self):
+		"""
+		Il est quelle heure ?
+		"""
+		return time.time()
+
+	def cool(self, a=3,b=2,c=4):
+		return (a,b,c)
+
+	def big_cool(self, *args, **kwargs):
+		"""
+		Une fonction avec un nombre variable de parametres positionnels
+		et nommés
+		"""
+		return kwargs
+
+
+service = zerobot.Service("myservice", "tcp://localhost:5001", MyService())
+service.start()
+
+
+
