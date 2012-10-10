03132644fa1c11f46db8f08ad039d2ef27580bf4
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-10-11 01:05:15 +0200

    ajout d'un vrai code d'example serveur python

--- examples/python/server/server.py
+++ examples/python/server/server.py
@@ -1,1 +1,11 @@
-../../../python3/scripts/server.py
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../../../python3')
+import zerobot
+import logging
+
+logging.basicConfig(level=20)
+
+server = zerobot.Server()
+server.start()
