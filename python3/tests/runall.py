2939df53e19464bbbf6727c343787aec5e5e6d49
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-11-04 22:31:59 +0100

    update runall.py

--- python3/tests/runall.py
+++ python3/tests/runall.py
@@ -3,5 +3,5 @@
 import os
 path = os.path.abspath(os.path.dirname(__file__))
 import subprocess
-p = subprocess.Popen('python3 -m unittest discover -p "test_*.py" -s %s -v' % path, shell=True)
+p = subprocess.Popen('python -m unittest discover -p "test_*.py" -s %s -v' % path, shell=True)
 p.wait()
