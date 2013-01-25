add5724d895f92765aa9cd8a267aa0110c64c277
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-01-26 00:39:23 +0100

    return same code as unittest

--- python3/tests/runall.py
+++ python3/tests/runall.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
+import sys
 import os
 path = os.path.abspath(os.path.dirname(__file__))
 import subprocess
 p = subprocess.Popen('python -m unittest discover -p "test_*.py" -s %s -v' % path, shell=True)
 p.wait()
+sys.exit(p.returncode)
